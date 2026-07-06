e4c1eaaab8175110e4a442b48a6c86ba5a346c95
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-06 08:06:18 +0000

    change allowed host to *

--- my_project_auth/my_project_auth/settings.py
+++ my_project_auth/my_project_auth/settings.py
@@ -18,7 +18,7 @@ load_dotenv()  # Load environment variables from .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]  # Allow all hosts for development; change this in production
 
 
 # Quick-start development settings - unsuitable for production
--- notifications/notifications/settings.py
+++ notifications/notifications/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Allow all hosts for development; change this in production
 
 
 # Application definition
