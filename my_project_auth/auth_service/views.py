d7d4517a775ac80124676a08c9f349c6f3880a16
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-07 16:38:48 +0000

    priavate key

--- my_project_auth/auth_service/views.py
+++ my_project_auth/auth_service/views.py
@@ -22,7 +22,7 @@ KEY_PAIR_ID = os.getenv("KEY_PAIR_ID")
 CDN_DOMAIN = os.getenv("CDN_DOMAIN")
 
 private_key= serialization.load_pem_private_key(
-    os.getenv("PRIVATE_KEY").encode(),
+    os.getenv("CLOUDFRONT_PRIVATE_KEY").encode(),
     password=None,
 )
 def rsa_signer(message):
