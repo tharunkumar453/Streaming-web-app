e3b6b57b3add8ac5d2e745f54eeaeecc48b7258e
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-07 11:09:44 +0000

    signed cookie set up

--- my_project_auth/auth_service/urls.py
+++ my_project_auth/auth_service/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import AddUserView, ResetPasswordView, LoginView
+from .views import AddUserView, ResetPasswordView, LoginView,RefreshCloudFrontCookieView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path("add-user/", AddUserView.as_view(), name="add_user"),
     path("reset-password/", ResetPasswordView.as_view(), name="reset_password"),
     path("login/", LoginView.as_view(), name="login"),
+    path("refresh-cloudfront-cookies/", RefreshCloudFrontCookieView.as_view(), name="refresh_cloudfront_cookies"),
+]
+    
+
+urlpatterns += [
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
--- my_project_auth/auth_service/views.py
+++ my_project_auth/auth_service/views.py
@@ -5,9 +5,55 @@ from rest_framework import status
 from .models import CustomUserModel
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
-
 from .tasks import EmailNotificationTask
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from botocore.signers import CloudFrontSigner
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+from datetime import datetime, timedelta, timezone
+from django.http import JsonResponse
+import base64
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+KEY_PAIR_ID = os.getenv("KEY_PAIR_ID")
+CDN_DOMAIN = os.getenv("CDN_DOMAIN")
+
+
+def rsa_signer(message):
+    return private_key.sign(
+        message,
+        padding.PKCS1v15(),
+        hashes.SHA1())
+
+with open("/workspaces/Streaming-web-app/my_project_auth/cloudfront_private_key.pem", "rb") as f:
+    private_key = serialization.load_pem_private_key(f.read(),password =None)
+    
+cloudfront_signer = CloudFrontSigner(KEY_PAIR_ID,rsa_signer)
+
+
+
+def cf_b64(data: bytes):
+    return (
+        base64.b64encode(data)
+        .decode()
+        .replace("+", "-")
+        .replace("=", "_")
+        .replace("/", "~")
+    )
+def generate_cloudfront_cookies(resource):
+    expire = datetime.now(timezone.utc) + timedelta(hours=3)
+    policy = cloudfront_signer.build_policy(resource,expire).encode()
+    signature = rsa_signer(policy)
+    return {
+        "CloudFront-Policy": cf_b64(policy),
+        "CloudFront-Signature": cf_b64(signature),
+        "CloudFront-Key-Pair-Id": KEY_PAIR_ID
+        }
+
+
 
 class AddUserView(APIView):
 
@@ -32,6 +78,7 @@ class AddUserView(APIView):
         return Response({"detail": "User registered successfully."}, status=status.HTTP_201_CREATED)
 
 
+
 class ResetPasswordView(APIView):
     def post(self, request):
         email = request.data.get("email")
@@ -51,24 +98,79 @@ class ResetPasswordView(APIView):
             return Response({"detail": "User with this email does not exist."}, status=status.HTTP_404_NOT_FOUND)
 
 
+
+
 class LoginView(APIView):
+
     def post(self, request):
-        email = request.data.get("email")  # Frontend sends as 'username'
+        email = request.data.get("email")
         password = request.data.get("password")
 
         if not email or not password:
-            return Response({"detail": "Email and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"detail": "Email and password are required."},status=status.HTTP_400_BAD_REQUEST)
 
         try:
             user = CustomUserModel.objects.get(email=email)
             if not user.check_password(password):
-                return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
-            
-            # Generate JWT tokens
+                return Response({"detail": "Invalid credentials."},status=status.HTTP_401_UNAUTHORIZED)
+
+            # Generate JWT Tokens
             refresh = RefreshToken.for_user(user)
-            return Response({
-                "access": str(refresh.access_token),
-                "refresh": str(refresh)
-            }, status=status.HTTP_200_OK)
+            access = refresh.access_token
+
+            # Create response
+            response = Response({
+                "access": str(access),
+                "refresh": str(refresh),
+                },
+                status=status.HTTP_200_OK,
+            )
+
+            # Generate CloudFront signed cookies
+            cookies = generate_cloudfront_cookies(f"{CDN_DOMAIN}/*")
+
+            for key, value in cookies.items():
+                response.set_cookie(
+                    key=key,
+                    value=value,
+                    secure=True,
+                    httponly=True,
+                    samesite="None",
+                    path="/",
+                    domain=".yourdomain.com"
+                )
+            return response
+
         except CustomUserModel.DoesNotExist:
-            return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"detail": "Invalid credentials."},status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        refresh_token = request.data.get("refresh")
+        if not refresh_token:
+            return Response({"detail": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # Blacklist the refresh token
+            return Response({"detail": "Logged out successfully."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RefreshCloudFrontCookieView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        cookies = generate_cloudfront_cookies(f"{CDN_DOMAIN}/*")
+        response = Response({ "message": "CloudFront cookies refreshed"}, status=status.HTTP_200_OK )  
+        for key, value in cookies.items():
+            response.set_cookie(
+                key,
+                value,
+                secure=True,
+                httponly=True,
+                samesite="None",
+                path="/"
+            )
+        return response
