04c52093b415a07520a3da65e1431bc89ebeb264
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-07 07:39:48 +0000

    change workflow

--- notifications/notifications/urls.py
+++ notifications/notifications/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("push.urls")),  # Include URLs from the push app
+    path("notifications/", include("push.urls")),  # Include URLs from the push app
 ]
