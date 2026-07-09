311a02ff1e1494e310abcc01df8385777c97d64f
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-09 11:42:53 +0000

    remove /

--- notifications/push/urls.py
+++ notifications/push/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path("api/devices/register/",RegisterDeviceAPIView.as_view()),
    
-    path("follow/<int:channel_id>/",Following_Follower_view.as_view()),
+    path("follow/<int:channel_id>",Following_Follower_view.as_view()),
     path("following/",Follwed_by_me.as_view())
 
 ]
