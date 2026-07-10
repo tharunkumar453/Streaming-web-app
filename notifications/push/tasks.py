aa50af5daa7d89ebf2bbbe4ee11e9d1e0d31777e
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-10 13:14:58 +0000

    fcm

--- notifications/push/tasks.py
+++ notifications/push/tasks.py
@@ -1,7 +1,15 @@
 from celery import shared_task
 from .models import DeviceNotification,Following_Follower
 import logging
-import notifications.firebase_config
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials
+
+if not firebase_admin._apps:
+    cred_dict = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
 
 from firebase_admin import messaging
 
