426203f282216c1efadce308d7b25f0ff784f894
tharunkumar453 <23021a0453@jntucek.ac.in>
2026-07-10 09:40:06 +0000

    cred

--- notifications/push/firebase_config.py
+++ notifications/push/firebase_config.py
@@ -3,7 +3,5 @@ from firebase_admin import credentials
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        "/workspaces/Streaming-web-app/notifications/notifications/ firebase-credintials.json"
-    )
+    cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
     firebase_admin.initialize_app(cred)
